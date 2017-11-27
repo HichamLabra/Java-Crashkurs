0f2f441472c385dac929ccca7e4cef62041fd4e5
Hicham Labrahimi <hicham.labrahimi@devoteam.com>
2017-11-27 20:05:53 +0100

    First Commit

---
+++ src/Variablen.java
@@ -0,0 +1,18 @@
+
+public class Variablen {
+
+	public static void main(String[] args) {
+		int zahl = 42; // +/- etwa 2 Milliarden
+		long groﬂeZahl = 1337L; // wirklich lang
+		short kleineZahle = 7; // -32.768 ... 32.767
+		byte sehrKleineZahl = -7; // -128 ... 127
+		
+		float kommaZahl = 3.14f; // weniger Nachkommastellen
+		double genauereKommaZahl = 3.14159d; // mehr Nachkommastellen
+		
+		char zeichen = 'F'; // ein Zeichen also Buchstabe/Zahl/Sonderzeichen
+		System.out.println(zeichen);
+
+	}
+
+}
